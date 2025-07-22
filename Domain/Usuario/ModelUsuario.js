72b2189a382f47840a49ba59e2f7476b88008efe
Luisivmaraz <luisivmaraz03@gmail.com>
2025-07-22 15:59:04 -0500

    feat(models): agregar modelos de luminarias, usuarios y seguridad

---
+++ Domain/Usuario/ModelUsuario.js
@@ -0,0 +1,29 @@
+class ModelUsuario {
+  constructor(
+    _id,
+    identificador,
+    nombre,
+    apellido,
+    telefono,
+    rol 
+  ) {
+    this._id = _id;
+    this.identificador = identificador;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.telefono = telefono;
+    this.rol = rol;
+  }
+}
+
+module.exports = { ModelUsuario };
+
+/**
+ * Modelo de los datos generales del usuario
+ * @param {_id} string - ID interno generado por la base de datos
+ * @param {identificador} string - Identificador único del usuario (consultas rapidas)
+ * @param {nombre} string - Nombre del usuario
+ * @param {apellido} string - Apellido del usuario
+ * @param {telefono} string - Teléfono de contacto
+ * @param {rol} string - Rol del usuario ('admin', 'supervisor', 'usuario')
+ */
